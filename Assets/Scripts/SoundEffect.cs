997c853364d2b5f373686be9e0151b3d41115fcd
Ivan Wang <icw@cmu.edu>
2015-01-25 11:44:11 -0500

    Add goop sound effect

---
+++ Assets/Scripts/SoundEffect.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(AudioSource))]
+public class SoundEffect : MonoBehaviour {
+
+  private bool isPlaying = false;
+
+	// Use this for initialization
+	void Start () {
+    //source = GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  void OnCollisionEnter2D(Collision2D collision) {
+    if (collision.gameObject.CompareTag("Player")) {
+      StartCoroutine("WaitAndPlaySound", audio.clip.length);
+    }
+  }
+
+  IEnumerator WaitAndPlaySound(float waitTime) {
+    if (!isPlaying) {
+      isPlaying = true;
+      audio.Play();
+      yield return new WaitForSeconds(waitTime);
+      isPlaying = false;
+    }
+  }
+}
