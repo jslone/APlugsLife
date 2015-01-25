9f7a45d83a4d129dcc47d0ab3191d47b1ca66f8b
Jacob M. Slone <jslone1993@gmail.com>
2015-01-25 08:43:02 -0500

    qssss and eeeees

---
+++ Assets/Scripts/GUIFade.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIFade : MonoBehaviour {
+	public float Delay = 3.0f;
+	public float FadeSpeed = 2.0f;
+
+	private CanvasGroup canvasGroup;
+	
+	private float timeUntilFade = 0.0f;
+	
+	// Use this for initialization
+	void Start () {
+		canvasGroup = GetComponent<CanvasGroup> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timeUntilFade < 0.0f) {
+			canvasGroup.alpha = Mathf.Max(0.0f,canvasGroup.alpha - FadeSpeed * Time.deltaTime);
+		} else {
+			timeUntilFade -= Time.deltaTime;
+		}
+	}
+	
+	public void Alert() {
+		timeUntilFade = Delay;
+		canvasGroup.alpha = 1.0f;
+	}
+}
--- Assets/Scripts/GUIRotate.cs
+++ Assets/Scripts/GUIRotate.cs
@@ -5,12 +5,10 @@ public class GUIRotate : MonoBehaviour {
 	public Transform rot;
 
 	private RectTransform rectTrans;
-	private CanvasGroup canvasGroup;
 
 	// Use this for initialization
 	void Start () {
 		rectTrans = GetComponent<RectTransform> ();
-		canvasGroup = GetComponent<CanvasGroup> ();
 	}
 	
 	// Update is called once per frame
