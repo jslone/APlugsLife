12aa143ed872f83b0cea0b03900c734a2eeb667a
Jacob M. Slone <jslone1993@gmail.com>
2015-01-24 22:58:30 -0500

    triggerable batteries

---
+++ Assets/Scripts/ActivateParent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Collider2D))]
+public class ActivateParent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		transform.parent.gameObject.rigidbody2D.isKinematic = false;
+	}
+}
