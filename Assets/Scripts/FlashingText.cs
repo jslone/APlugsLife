fd19ce1a3f530fe8f57817f1e7bb2cb586449200
Ivan Wang <icw@cmu.edu>
2015-01-25 04:38:26 -0500

    Title screen with flashy text

---
+++ Assets/Scripts/FlashingText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+ 
+public class FlashingText : MonoBehaviour {
+  Text flashingText;
+  string textToFlash;
+
+  void Start() {
+    flashingText = GetComponent<Text>();
+    textToFlash = flashingText.text;
+    StartCoroutine(BlinkText());
+  }
+
+  public IEnumerator BlinkText() {
+    while (true) {
+      flashingText.text = "";
+      // display blank text for 0.5 seconds
+      yield return new WaitForSeconds(.5f);
+      // display text for the next 0.5 seconds
+      flashingText.text = textToFlash;
+      yield return new WaitForSeconds(.5f);
+    }
+  }
+}
