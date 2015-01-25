83917114c1851538dc367a712ed3c2fbfe00e3bf
Jacob M. Slone <jslone1993@gmail.com>
2015-01-25 06:17:45 -0500

    colors n stuff

---
+++ Assets/Scripts/ColorAngle.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorAngle : MonoBehaviour {
+	public static Color[] colors = new Color[] {
+		new Color(248, 132, 8, 255)/255,
+		new Color(38, 89, 245, 255)/255,
+		new Color(12, 33, 43, 255)/255,
+		new Color(24, 48, 113, 255)/255,
+		new Color(73, 211, 111, 255)/255,
+		new Color(137, 15, 2, 255)/255,
+		new Color(23, 96, 92, 255)/255 
+	};
+	public static float index;
+	public static bool blend = true;
+	public float potency = 1.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float fdx = index * colors.Length;
+		int idx = Mathf.FloorToInt (fdx);
+		Color c;
+		if(blend) {
+			c = Color.Lerp(colors[idx],colors[(idx+1)%colors.Length],fdx-idx);
+		} else {
+			c = colors [idx];
+		}
+		renderer.material.color = Color.Lerp(Color.white,c,potency);
+	}
+}
--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -10,7 +10,9 @@ public class Line : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Setup ();
+		lrend = GetComponent<LineRenderer> ();
+		lrend.SetWidth (1.0f, 1.0f);
+		lrend.SetVertexCount (links.Length);
 	}
 	
 	// Update is called once per frame
@@ -18,52 +20,5 @@ public class Line : MonoBehaviour {
 		for(int i = 0; i < links.Length; i++) {
 			lrend.SetPosition(i,links[i].position);
 		}
-		if(breakable) {
-			Break ();
-		}
 	}
-
-	void FixedUpdate() {
-		if(breakable) {
-			for(int i = 0; i < links.Length - 1; i++) {
-				if(Physics2D.Linecast(links[i].position,links[i+1].position,mask)) {
-					Break ();
-				}
-			}
-		}
-	}
-
-	void Break() {
-		Transform[] a, b;
-		int len = links.Length / 2;
-		a = new Transform[len];
-		b = new Transform[links.Length - len];
-		for(int i = 0; i < len; i++) {
-			a[i] = links[i];
-		}
-		for(int i = len; i < links.Length; i++) {
-			b[i - len] = links[i];
-		}
-
-		if(len - 1 >= 0) {
-			links[len-1].GetComponent<Joint2D>().connectedBody = null;
-		}
-
-		links = a;
-		breakable = false;
-		Setup ();
-
-		Line n = ((GameObject)Instantiate (gameObject)).GetComponent<Line> ();
-		n.links = b;
-		n.breakable = false;
-		n.Setup ();
-	}
-
-	public void Setup() {
-		lrend = GetComponent<LineRenderer> ();
-		lrend.SetWidth (1.0f, 1.0f);
-		lrend.SetVertexCount (links.Length);
-	}
-
-	
 }
