c25973c40a2214b08a289c4d3caa3c9ee155dbe5
Jacob M. Slone <jslone1993@gmail.com>
2015-01-25 06:59:38 -0500

    flags

---
+++ Assets/Scripts/SpriteToggle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(SpriteRenderer),typeof(Collider2D))]
+public class SpriteToggle : MonoBehaviour {
+	SpriteRenderer rendr;
+	public Sprite on;
+	public Sprite off;
+
+	// Use this for initialization
+	void Start () {
+		rendr = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		rendr.sprite = on;
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		rendr.sprite = off;
+	}
+}
