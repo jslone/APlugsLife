8f2b34357d020a6c7b568535b7788f8258ae41d6
Jacob M. Slone <jslone1993@gmail.com>
2015-01-25 12:03:45 -0500

    cutscenes n stuff

---
+++ Assets/Scripts/GUIShrink.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(RectTransform))]
+public class GUIShrink : MonoBehaviour {
+	RectTransform rtrans;
+
+	float speedFactor = 0.9f;
+	bool shrinking = false;
+
+	public float Delay = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+		rtrans = GetComponent<RectTransform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(shrinking && rtrans.localScale.x > 10) {
+			if(Delay < 0) {
+				rtrans.localScale *= speedFactor;
+			} else {
+				Delay -= Time.deltaTime;
+			}
+		}
+	}
+
+	public void Alert() {
+		shrinking = true;
+	}
+}
